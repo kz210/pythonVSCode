91c003ea24b2a680609ba186b65673c671b5da9c
kz210 <kun.zhao@hotmail.co.uk>
2020-09-21 00:42:10 +0100

    solve matplotlib issue of backend by pyqt5 installation

---
+++ plot_test.py
@@ -0,0 +1,7 @@
+import matplotlib
+import matplotlib.pyplot as plt #need to install pyqt5 for backend() in vscode
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
